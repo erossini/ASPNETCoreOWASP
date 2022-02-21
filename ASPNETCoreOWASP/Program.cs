f17dee6ad1204e3c1d9a07b60716a9b8c8ef0179
Enrico Rossini <enrico.rossini.uk@live.com>
2022-02-21 12:38:41 +0000

    Add project

---
+++ ASPNETCoreOWASP/Program.cs
@@ -0,0 +1,49 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.Use((context, next) =>
+{
+    context.Response.GetTypedHeaders().CacheControl =
+        new Microsoft.Net.Http.Headers.CacheControlHeaderValue()
+        {
+            MustRevalidate = true,
+            NoCache = true,
+            NoStore = true,
+
+        };
+
+    context.Response.Headers.Add("X-Content-Type-Options", "nosniff");
+    context.Response.Headers.Add("Content-Security-Policy", 
+        "default-src 'none'; " + "script-src 'self'; " + 
+        "connect-src 'self'; " + 
+        "img-src 'self'; " + 
+        "style-src 'self'; " +
+        "base-uri 'self'; " + 
+        "form-action 'self'; " + 
+        "frame-ancestors 'none';");
+
+    return next.Invoke();
+});
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
